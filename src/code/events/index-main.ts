944d1c7855ab568ce8c0472e92d9113e99c0f8e2
Tertius Roach <tertius.roach@gmail.com>
2022-10-06 10:51:35 +0200

    Refined rain-main

--- src/code/events/index-main.ts
+++ src/code/events/index-main.ts
@@ -1,5 +1,5 @@
 export namespace IndexMain {
-  export function eventsFor(pageName: String | 'default-main' | 'opdatering-main' | 'rain-main' | 'rooster-main' | 'statistieke-main') {
+  export function eventsFor(pageName: String | 'default-main' | 'opdatering-main' | 'rooster-main' | 'statistieke-main' | 'rain-main') {
     const indexBody: HTMLElement = document.getElementById('index-body');
     const indexHeader: HTMLElement = document.getElementById('index-header');
     const indexMain: HTMLElement = document.getElementById('index-main');
@@ -41,11 +41,42 @@ export namespace IndexMain {
         };
         toggleHeader(indexMain);
         break;
-      case 'rain-main':
-        break;
       case 'rooster-main':
         break;
       case 'statistieke-main':
+        break;
+      case 'rain-main':
+        //--|▼| Adds rain effect |▼|--//
+        const rainEffect = (indexMain: HTMLElement) => {
+          let topContainer: HTMLElement = document.querySelector('body .rain-main .rain-top');
+          let bottomContainer: HTMLElement = document.querySelector('body .rain-main .rain-bottom');
+          function addRain(container: HTMLElement) {
+            let i = 0;
+            let amount = 25;
+            while (i < amount) {
+              var drop = document.createElement('i');
+
+              var size = Math.random() * 5;
+              var posX = Math.floor(Math.random() * window.innerWidth);
+
+              var delay = Math.random() * -20;
+              var duration = Math.random() * 5;
+
+              drop.style.width = 0.2 + size + 'px';
+              drop.style.left = posX + 'px';
+              drop.style.animationDelay = delay + 's';
+              drop.style.animationDuration = 4 + duration + 's';
+
+              container.appendChild(drop);
+
+              i++;
+            }
+          }
+          addRain(topContainer);
+          addRain(bottomContainer);
+        };
+        rainEffect(indexMain);
+        console.log(`--${pageName} Loaded`);
         break;
     }
     //--► console.log(`--${pageName} Loaded`); ◄--//
