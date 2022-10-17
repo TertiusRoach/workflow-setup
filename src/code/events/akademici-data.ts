2f0894ec4954cf117c45c39d0b796206ca1adf3a
Tertius Roach <tertius.roach@gmail.com>
2022-10-17 13:02:40 +0200

    Added loading-screen to akademici

--- src/code/events/akademici-data.ts
+++ src/code/events/akademici-data.ts
@@ -1,7 +1,15 @@
 export namespace AkademiciData {
   export function eventsFor(pageName: String | 'default-data') {
+    const akademiciBody: HTMLElement = document.getElementById('akademici-body');
+    const akademiciHeader: HTMLElement = document.getElementById('akademici-header');
+    const akademiciMain: HTMLElement = document.getElementById('akademici-main');
+    const akademiciSidebar: HTMLElement = document.getElementById('akademici-sidebar');
+    const akademiciOverlay: HTMLElement = document.getElementById('akademici-overlay');
+    const akademiciFooter: HTMLElement = document.getElementById('akademici-footer');
+    const akademiciData: HTMLElement = document.getElementById('akademici-data');
     switch (pageName) {
       case 'default-data':
+        akademiciBody.querySelector('#loading-screen').remove();
         break;
     }
     //--► console.log(`--${pageName} Loaded`); ◄--//
