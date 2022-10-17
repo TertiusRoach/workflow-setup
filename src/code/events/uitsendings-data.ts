f31356635d9a850b623f88c660e121748293b9b0
Tertius Roach <tertius.roach@gmail.com>
2022-10-17 14:24:59 +0200

    Added uitsending loading screen

--- src/code/events/uitsendings-data.ts
+++ src/code/events/uitsendings-data.ts
@@ -1,7 +1,15 @@
 export namespace UitsendingsData {
+  const uitsendingsBody: HTMLElement = document.getElementById('uitsendings-body');
+  const uitsendingsHeader: HTMLElement = document.getElementById('uitsendings-header');
+  const uitsendingsMain: HTMLElement = document.getElementById('uitsendings-main');
+  const uitsendingsSidebar: HTMLElement = document.getElementById('uitsendings-sidebar');
+  const uitsendingsOverlay: HTMLElement = document.getElementById('uitsendings-overlay');
+  const uitsendingsFooter: HTMLElement = document.getElementById('uitsendings-footer');
+  const uitsendingsData: HTMLElement = document.getElementById('uitsendings-data');
   export function eventsFor(pageName: String | 'default-data') {
     switch (pageName) {
       case 'default-data':
+        uitsendingsBody.querySelector('#loading-screen').remove();
         break;
     }
     //--► console.log(`--${pageName} Loaded`); ◄--//
