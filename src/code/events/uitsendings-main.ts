ba6ba2dfd7de010597aaca9c08b40138253a66e2
Tertius Roach <tertius.roach@gmail.com>
2022-11-03 14:22:40 +0200

    Back-end Copying Working

--- src/code/events/uitsendings-main.ts
+++ src/code/events/uitsendings-main.ts
@@ -11,23 +11,6 @@ export namespace UitsendingsMain {
 
     switch (pageName) {
       case 'login-main':
-        /*
-        <script type="text/javascript">
-        function onSignIn(googleUser) {
-          var profile = googleUser.getBasicProfile();
-          console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-          console.log('Name: ' + profile.getName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-        }
-        function signOut() {
-          var auth2 = gapi.auth2.getAuthInstance();
-          auth2.signOut().then(function () {
-            console.log('User signed out.');
-          });
-        }
-      </script>
-      */
         break;
       case 'opdatering-main':
         //--|▼| Change header date to present |▼|--//
